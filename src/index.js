7b56c685c4fcbe4c5c49463f69528f6c172580fe
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-07-27 18:26:14 +0300

    leaderboard skeleton

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+
+import ListObj from './modules/display';
+import { listObj } from './modules/object';
+
+ListObj.displayAny(listObj);
---
+++ src/modules/object.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+
+const listObj = [
+  {
+    name: 'Hamxie',
+    scores: 20000,
+  },
+  {
+    name: 'Goodboy',
+    scores: 20000,
+  },
+  {
+    name: 'SaintCJ',
+    scores: 20000,
+  },
+  {
+    name: 'Hurricane',
+    scores: 20000,
+  },
+];
+
+export { listObj };
